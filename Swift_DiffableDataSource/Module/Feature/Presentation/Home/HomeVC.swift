4b791d5c299d59d966d3bed206b3395d91b19f78
Yslee-dev-01 <ysleedev01@gmail.com>
2025-10-13 16:31:03 +0900

    Feat(Home): HomeVC, ViewModel 파일 추가

--- Swift_DiffableDataSource/Module/Feature/Presentation/Home/HomeVC.swift
+++ Swift_DiffableDataSource/Module/Feature/Presentation/Home/HomeVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeVC.swift
 //  Swift_DiffableDataSource
 //
 //  Created by 이윤수 on 10/13/25.
@@ -7,12 +7,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class HomeVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         view.backgroundColor = .white
     }
 }
-
